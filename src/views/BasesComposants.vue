b4ba44589f52959329c0e67f67c91d734c21ce98
Dylane <tanolamene@gmail.com>
2024-10-27 12:36:34 -0300

    Start Chapitre 7 : Les bases des composants

--- src/views/BasesComposants.vue
+++ src/views/BasesComposants.vue
@@ -2,6 +2,9 @@
   <section class="padd content-limit">
     <div>
       <h2>Chapitre 7 : Les bases des composants</h2>
+      <div class="lesson">
+        <h3></h3>
+      </div>
     </div>
   </section>
 </template>
