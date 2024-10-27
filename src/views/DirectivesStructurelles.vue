0b21decd37d61e0011515b964fd5e486f07f3c72
Dylane <tanolamene@gmail.com>
2024-10-27 11:23:09 -0300

    Start Chapitre 6 : Les directives structurelles

---
+++ src/views/DirectivesStructurelles.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="padd content-limit">
+    <div>
+      <h2>Chapitre 6 : Les directives structurelles</h2>
+      <div class="lesson">
+        <h3>Leçon 31 : Affichage conditionnel avec les directives v-if, v-else et v-else-if</h3>
+      </div>
+      <div class="lesson">
+        <h3>Leçon 32 : Les directives v-show et v-pre</h3>
+      </div>
+      <div class="lesson">
+        <h3>Leçon 33 : La directive v-for</h3>
+      </div>
+      <div class="lesson">
+        <h3>Leçon 34 : Directive v-for, détection des changements et combinaison avec v-if</h3>
+      </div>
+      <div class="lesson">
+        <h3>Leçon 35 : Les directives v-once et v-memo</h3>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
