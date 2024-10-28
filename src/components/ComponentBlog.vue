23c46108c6c603182f78abed756502299a348273
Dylane <tanolamene@gmail.com>
2024-10-28 11:17:46 -0300

    End Chapitre 7 : Les bases des composants

---
+++ src/components/ComponentBlog.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- <button @click="emit('bigger', props.fontSize, 5)">+</button> -->
+  <button @click="emit('bigger', 5)">+</button>
+  <button @click="emit('makeSmaller')">-</button>
+  <h3>Un titre d'article</h3>
+  <p :style="{ fontSize: props.fontSize + 'px' }">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat similique
+    ea animi minima ducimus quo blanditiis sequi sed, facere possimus
+    reprehenderit id explicabo quaerat aspernatur repellendus rem, consequuntur
+    culpa illo.
+  </p>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref, type Ref } from 'vue'
+
+// Par defaut les propriete sont en read only
+const props = defineProps<{
+  fontSize: number
+}>()
+
+// console.log(props.fontSize)
+// const emit = defineEmits(['bigger']);
+// const emit = defineEmits({
+//   bigger: (fontSize: number, inc:number) => {
+//     console.log(inc,fontSize);
+
+//     return fontSize > 30 ? false : true;
+//   },
+//   // bigger: (inc:number) => {
+//   //   return true;
+//   // }
+// })
+
+//synatxe plus elegante
+const emit = defineEmits<{
+  (e: 'bigger', inc: number): void
+  (e: 'makeSmaller'): void
+}>()
+
+//Choisir ce qui peut etre exposer
+
+const count = ref(15)
+const arrTest = reactive([1, 2, 3])
+function display() {
+  console.log('Display')
+}
+
+defineExpose<{ count: Ref<number>; arrTest: number[]; display: () => void }>({
+  count,
+  arrTest,
+  display,
+})
+</script>
+
+<style scoped lang="scss"></style>
--- src/views/BasesComposants.vue
+++ src/views/BasesComposants.vue
@@ -21,40 +21,88 @@
         <!-- <Product :prix="1800" :name="product" available/>
         <Product :prix="500" :name="product2" available /> -->
         <Product :product="product" />
-         <!-- <product v-bind="product"/> -->
+        <!-- <product v-bind="product"/> -->
       </div>
       <div class="lesson">
         <h3>Leçon 40 : Les événements</h3>
+        <!-- <Blog @bigger="incSize" :fontSize="fontSize" /> -->
+        <Blog @bigger="incSize" @make-smaller="decrease" :fontSize="fontSize" />
       </div>
       <div class="lesson">
         <h3>Leçon 41 : Le cycle de vie d'un composant</h3>
       </div>
       <div class="lesson">
         <h3>Leçon 42 : Les références de template</h3>
+        <h4>Form</h4>
+        <input type="text" placeholder="Nom" ref="input" />
+        <ul>
+          <li ref="fruitsRef" v-for="fruit in fruits" :key="fruit">
+            {{ fruit }}
+          </li>
+        </ul>
       </div>
       <div class="lesson">
-        <h3>Leçon 43 : Utilisation des liaisons et des directives sur les composants</h3>
+        <h3>
+          Leçon 43 : Utilisation des liaisons et des directives sur les
+          composants
+        </h3>
+        <Blog @bigger="incSize" @make-smaller="decrease" :fontSize="fontSize" class="parent" />
       </div>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
-import Product from '@/components/ComponentProduct.vue';
-import type { ProductInterface } from '@/interfaces/product.interface';
-import { reactive} from 'vue';
+import Product from '@/components/ComponentProduct.vue'
+import type { ProductInterface } from '@/interfaces/product.interface'
+import { onMounted, reactive, ref } from 'vue'
 
 // const product = ref("Voiture")
 // const product2 = ref("Banane")
 
 const product = reactive<ProductInterface>({
   available: true,
-  prix:1500,
-  name: "Moto"
+  prix: 1500,
+  name: 'Moto',
 })
+// ========================
+import Blog from '@/components/ComponentBlog.vue'
 
+const fontSize = ref(16)
+
+// function incSize(f:number,inc:number){
+//   fontSize.value += inc;
+// }
+function incSize(inc: number) {
+  fontSize.value += inc
+}
+function decrease() {
+  fontSize.value--
+}
+//==================================
+
+const input = ref<HTMLInputElement | null>(null)
+
+onMounted(() => {
+  console.log(input.value?.focus())
+  console.log(fruitsRef)
+})
+const fruits = reactive(['pomme', 'banane', 'orange'])
+const fruitsRef = ref<HTMLLIElement[] | []>([])
+
+const blogRef = ref<InstanceType<typeof Blog> | null>(null)
+
+onMounted(() => {
+  console.log(input.value?.focus)
+  console.log(fruitsRef.value)
+  console.log(blogRef.value?.count)
+  console.log(blogRef.value?.arrTest)
+  blogRef.value?.display()
+})
 </script>
 
 <style scoped lang="scss">
-
+input {
+  padding: 1rem;
+}
 </style>
