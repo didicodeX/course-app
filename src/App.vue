cdaa162cbd2678056123aac031a7cf0d0eb0d574
Dylane <tanolamene@gmail.com>
2024-11-01 06:12:34 -0300

    End Chapitre 9

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,23 @@
 </template>
 
 <script setup lang="ts">
+// import {provide, readonly} from "vue"
 import HelloWorld from './components/HelloWorld.vue'
 import './assets/base.css'
 import Navbar from './components/NavBar.vue'
+
+// //============= 61 ====================
+// export interface MangakaInterface{
+//   name: string;
+//   birthday: number;
+// }
+
+// const mangaka = {
+//   name: "Dylane",
+//   birthday:2005
+// }
+
+// provide('mangaka',readonly(mangaka))
 </script>
 
 <style scoped lang="scss">
--- src/views/AvanceesComposant.vue
+++ src/views/AvanceesComposant.vue
@@ -41,12 +41,20 @@
       </div>
       <div class="lesson">
         <h3>Leçon 61 : Provide et Inject</h3>
+        <input type="text" v-model="mangaka.name">
+        <AuthorMangas></AuthorMangas>
       </div>
       <div class="lesson">
         <h3>Leçon 62 : Utilisation des symboles avec provide et inject</h3>
       </div>
       <div class="lesson">
         <h3>Leçon 63 : Les fonctions utilitaires de l'API réactivité</h3>
+        <button @click="compteur++">{{ compteur }}</button>
+        <button @click="count++">{{ count }}</button>
+        <pre>
+          {{ state }}
+        </pre>
+        <p>{{ user.name }}</p>
       </div>
       <div class="lesson">
         <h3>Leçon 64 : Les composants asynchrones</h3>
@@ -56,9 +64,12 @@
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue"
+
+import {provide, reactive, readonly, ref, toRef} from "vue"
 import Article from "@/components/ComponentArticle.vue";
 import AppButton from "@/components/AppButton.vue";
+import AuthorMangas from "@/components/AuthorMangas.vue";
+import { mangakaKey } from "@/provideKeys";
 
 // const content = ref("");
 const content = ref(true);
@@ -67,8 +78,59 @@ const name = ref("");
 //========= losson 59 =======
 const author = ref("Jean de la tour")
 const bottom = "cta";
-</script>
 
+//============= 61 ====================
+
+
+//============= 61 ====================
+export interface MangakaInterface{
+  name: string;
+  birthday: number;
+}
+
+const mangaka = reactive({
+  name: "Dylane",
+  birthday:2005
+})
+
+function updateBirthday(){
+  mangaka.birthday = 2000;
+}
+
+// provide('mangaka',readonly(mangaka))
+// ==> consulter la balise script plus haut
+
+//recommander
+provide(mangakaKey,{
+  mangaka: readonly(mangaka),
+  updateBirthday
+})
+
+//pas tres recommandable
+// provide('mangaka',{
+//   mangaka: readonly(mangaka),
+//   updateBirthday
+// })
+
+
+console.log(mangaka);
+
+
+//=========== 63 ============
+
+const {compteur, user} = reactive({
+  compteur: 0,
+  user: {name: "Dylane"}
+})
+
+const state = reactive({
+  count:0,
+  user:{}
+})
+
+const count = toRef(state,'count');
+
+</script>
 
 <style scoped lang="scss"></style>
 
